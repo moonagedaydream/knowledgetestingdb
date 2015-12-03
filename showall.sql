502dfaa29aca10e0a6509f6df2ac4ee7715efbc0
moonagedaydream <thetasigma@mail.ru>
2015-12-03 20:30:57 +0300

    Create showall.sql

---
+++ showall.sql
@@ -0,0 +1,24 @@
+--//-------------------------------------------------------
+--//---вывод-результатов-----------------------------------
+--//-------------------------------------------------------
+
+SELECT * FROM Tests
+GO
+SELECT * FROM Subtests
+GO
+SELECT * FROM Questions
+GO
+SELECT * FROM Answers
+GO
+SELECT * FROM Users
+GO
+SELECT * FROM User_tests
+GO
+SELECT * FROM User_subtests
+GO
+SELECT * FROM User_answers
+GO
+SELECT * FROM [All Open Questions TO Check]
+GO
+SELECT * FROM [All Spoken Questions TO Check]
+GO
